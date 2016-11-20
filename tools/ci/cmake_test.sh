eb7c4e2fce5ca5c8afbfe2c979da11cf74af436c
Lucas Vickers <lucasvickers@gmail.com>
2016-11-20 18:59:27 -0500

    disabling verbose

--- tools/ci/cmake_test.sh
+++ tools/ci/cmake_test.sh
@@ -4,13 +4,13 @@ set -e
 # build debug
 mkdir build-debug
 cd build-debug
-cmake .. -DCINDER_VERBOSE:BOOL=1 -DCMAKE_BUILD_TYPE=Debug -DCINDER_BUILD_TESTS=1 -DCINDER_BUILD_ALL_SAMPLES=1
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCINDER_BUILD_TESTS=1 -DCINDER_BUILD_ALL_SAMPLES=1
 make -j4
 cd ..
 # build release
 mkdir build-release
 cd build-release
-cmake .. -DCINDER_VERBOSE:BOOL=1 -DCMAKE_BUILD_TYPE=Release -DCINDER_BUILD_TESTS=1 -DCINDER_BUILD_ALL_SAMPLES=1
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCINDER_BUILD_TESTS=1 -DCINDER_BUILD_ALL_SAMPLES=1
 make -j4
 # test release
 make check
